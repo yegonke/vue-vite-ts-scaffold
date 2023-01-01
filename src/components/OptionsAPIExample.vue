9f472735b73f451812163681906abb01f9e16c41
Kipngetich Yegon <emanuelyegon@gmail.com>
2023-01-01 20:19:17 +0300

    First commit

---
+++ src/components/OptionsAPIExample.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  methods: {
+    handleChange(event) {
+      // `event` implicitly has `any` type
+      console.log(event.target.value)
+    }
+  }
+})
+</script>
+
+<template>
+  <input type="text" @change="handleChange" />
+</template>
